e07cac2cfba48a485ab471dd17d3193edfa74a43
Bytyqi Erson <Erson.Bytyqi@swisscom.com>
2023-02-06 09:57:22 +0100

    renamed classes and formattet everything

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import "../assets/css/home.css";
 
 function Home() {
   return (
-    <div className="home-of-electric-pattern">
-      <div className="diagonal-div">
-        <div className="container-text-positioning">
-          <p className="welcome-to">Welcome to the</p>
-          <h1 className="title-home-of-electric">Home of Electric</h1>
+    <div className="home-logo-pattern">
+      <div className="home-diagonal-div">
+        <div className="home-container-text">
+          <p className="home-welcome-to">Welcome to the</p>
+          <h1 className="home-title">Home of Electric</h1>
         </div>
       </div>
     </div>
